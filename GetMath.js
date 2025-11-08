1e0e4d6ae3c0a9e9e12b245f34f3af4c9d8e4d85
Danf-5000 <saniaiii@yandex.ru>
2025-11-08 11:07:51 +0400

    Add files via upload

---
+++ GetMath.js
@@ -0,0 +1,43 @@
+function Plus(a, b) {
+    return a + b;
+}
+
+function Minus(a, b) {
+    return a - b;
+}
+
+function Multiply(a, b) {
+    return a * b;
+}
+
+function Divide(a, b) {
+    if (b === 0) {
+        throw new Error('Ошибка: деление на ноль невозможно!');
+    }
+    return a / b;
+}
+
+function Degree(a, b) {
+    return Math.pow(a, b);
+}
+
+function Sin(a) {
+         return Math.sin(a);
+}
+
+function Cos(a) {
+         return Math.cos(a);
+}
+
+function Tan(a) {
+         return Math.tan(a);
+}
+function RectangleP(a, b) {
+         return a*2 + b*2;
+}
+function TriangleP(a, b, c) {
+         return a + b + c;
+}
+function CircleP(r) {
+         return 2 * Math.PI * r;
+}
